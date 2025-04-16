ac27003b662f3eabaa06423a472490433788da32
fewarren <FWarren574@aol.com>
2025-04-16 08:51:02 -0700

    Update ExtractVideoFramesFactory.php

--- VideoThumbnail/src/Service/ControllerPlugin/ExtractVideoFramesFactory.php
+++ VideoThumbnail/src/Service/ControllerPlugin/ExtractVideoFramesFactory.php
@@ -9,9 +9,15 @@ class ExtractVideoFramesFactory implements FactoryInterface
 {
     public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
     {
-        $settings = $services->get('Omeka\Settings');
-        $ffmpegPath = $settings->get('videothumbnail_ffmpeg_path', '/usr/bin/ffmpeg');
-        $videoFrameExtractor = new \VideoThumbnail\Stdlib\VideoFrameExtractor($ffmpegPath);
+        // Attempt to retrieve VideoFrameExtractor from the service container
+        if ($services->has('VideoThumbnail\Stdlib\VideoFrameExtractor')) {
+            $videoFrameExtractor = $services->get('VideoThumbnail\Stdlib\VideoFrameExtractor');
+        } else {
+            // Fallback to direct instantiation if not registered
+            $settings = $services->get('Omeka\Settings');
+            $ffmpegPath = $settings->get('videothumbnail_ffmpeg_path', '/usr/bin/ffmpeg');
+            $videoFrameExtractor = new \VideoThumbnail\Stdlib\VideoFrameExtractor($ffmpegPath);
+        }
         
         return new ExtractVideoFrames($videoFrameExtractor);
     }
