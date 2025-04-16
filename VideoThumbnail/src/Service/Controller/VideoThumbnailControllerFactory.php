c0f443a82b593be5467ef20b09649e789019b31d
fewarren <FWarren574@aol.com>
2025-04-16 08:54:09 -0700

    Update VideoThumbnailControllerFactory.php

--- VideoThumbnail/src/Service/Controller/VideoThumbnailControllerFactory.php
+++ VideoThumbnail/src/Service/Controller/VideoThumbnailControllerFactory.php
@@ -10,7 +10,6 @@ class VideoThumbnailControllerFactory implements FactoryInterface
     public function __invoke(ContainerInterface $services, $requestedName, array $options = null)
     {
         $entityManager = $services->get('Omeka\EntityManager');
-        $config = $services->get('Config');
         $settings = $services->get('Omeka\Settings');
         
         // Try to get file manager using a more robust approach
